c5f0f59b7d987bfea4d1f5258859fe2578cc92da
Grishmaprabha <greeshmaprabha1502@gmail.com>
2023-10-13 09:40:15 +0530

    changes

--- project_management/dashboard/views.py
+++ project_management/dashboard/views.py
@@ -607,7 +607,7 @@ def send_email_reminder():
     except:
         return HttpResponse("Email sent Unsuccessful!")
 
-# send_email_reminder()
+send_email_reminder()
 
 # def schedule_email_reminder(request):
 #     send_email_reminder.delay()
